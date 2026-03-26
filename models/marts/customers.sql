505d89e0b62c5864297b6f7d2cdd839666af568a
chris ballenger <chris.ballenger@dbtlabsworkshop.com>
2026-03-26 20:48:07 +0000

    day 2 training comp[lete

--- models/marts/customers.sql
+++ models/marts/customers.sql
@@ -1,12 +1,10 @@
 with customers as (
 
     select
-        id as customer_id,
+        customer_id,
         first_name,
         last_name
 
-    -- from raw.jaffle_shop.customers
-    -- from {{source('jaffle_shop', 'customers')}}
     from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
@@ -14,13 +12,12 @@ with customers as (
 orders as (
 
     select
-        id as order_id,
-        user_id as customer_id,
+        order_id,
+        customer_id,
         order_date,
         status
 
-    -- from raw.jaffle_shop.orders
-    from {{source('jaffle_shop', 'orders')}}
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
